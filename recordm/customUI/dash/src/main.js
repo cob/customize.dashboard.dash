b77c117f0b9884fea9ad03835859f351d76adab8
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2023-02-15 10:17:46 +0000

    Trigger the destroy of the dash when the name of the dash changes

--- recordm/customUI/dash/src/main.js
+++ recordm/customUI/dash/src/main.js
@@ -1,11 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Vue from "vue";
+import App from "./App.vue";
 import "./output.css";
-import './assets/css/all.min.css';
+import "./assets/css/all.min.css";
 
-new Vue({
-  render: function (h) { return h(App) }
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+let vueApp = new Vue({
+    render: function(h) { return h(App); },
+}).$mount("#app");
+
+function getDashName() {
+    const dashParts = window.location.hash.split("/")
+    return dashParts[dashParts.length - 1].split("?")[0]
+}
+
+const dashName = getDashName()
+
+function onHashChange() {
+    const currentDashName = getDashName()
+    if (currentDashName !== dashName) {
+        console.debug("Leaving the dashboard. Destroying the dashboard");
+        vueApp.$destroy();
+        window.removeEventListener("hashchange", onHashChange, true);
+    }
+}
+
+window.addEventListener("hashchange", onHashChange, true);
