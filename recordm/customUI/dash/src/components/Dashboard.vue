d3cbb79fa6dbb1257fb469896c0dfbbb94f443f9
Miguel Mesquita <mm@cob.pt>
2023-05-23 15:34:08 +0100

    feat: add support for dashboard menus

--- recordm/customUI/dash/src/components/Dashboard.vue
+++ recordm/customUI/dash/src/components/Dashboard.vue
@@ -1,7 +1,13 @@
 <template>
-    <div :class="classes" :style="image" >
-        <div :class="width + ' ' + grid">
-            <Board v-for="(board,i) in boards" :board="board" :key="i" />
+    <div class="h-full" :class="classes" :style="image" >
+        <div v-if="menu" class="flex flex-row flex-wrap justify-end space-x-2 mb-10"> 
+            <a v-for="(item,i) in menu" :key="item.name+i" :href="item.href" :class="item.active?'bg-lime-100':'bg-white'" class="rounded  py-1 px-2 text-xs min-w-fit whitespace-nowrap font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-lime-100" v-html="item.name"></a>
+        </div>
+        
+        <div  >
+            <div :class="width + ' ' + grid">
+                <Board v-for="(board,i) in boards" :board="board" :key="i" />
+            </div>
         </div>
     </div>
 </template>
@@ -12,7 +18,8 @@
     export default {
         components: { Board },
         props: {
-          dashboard: Object
+          dashboard: Object,
+          menu: Array
         },
         computed: {
             options() { return this.dashboard['DashboardCustomize'][0] },
