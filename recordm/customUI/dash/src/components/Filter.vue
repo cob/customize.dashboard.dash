83d55becf343be7ec5917070be002dd894c16495
Miguel Mesquita <mm@cob.pt>
2023-05-31 15:55:02 +0100

    style(filter): better size

--- recordm/customUI/dash/src/components/Filter.vue
+++ recordm/customUI/dash/src/components/Filter.vue
@@ -9,7 +9,7 @@
             @keyup="resize"
             :placeholder="placeholder"
         ></textarea>
-        <button @click="activateFromInputChange" type="submit" class="max-h-11 p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800">
+        <button @click="activateFromInputChange" type="submit" class="max-h-11 p-2 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
     </div>
