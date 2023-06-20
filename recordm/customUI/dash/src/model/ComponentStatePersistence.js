54335ea933cfbbeeaaea974144326b88294ccbd5
Miguel Mesquita <mm@cob.pt>
2023-06-20 15:40:12 +0100

    fix: better detection of hash args change

--- recordm/customUI/dash/src/model/ComponentStatePersistence.js
+++ recordm/customUI/dash/src/model/ComponentStatePersistence.js
@@ -1,13 +1,15 @@
-const DEBUG = false
+const DEBUG = true
 class ComponentStatePersistence {
     constructor(id,updateCb) {
         this._id = id
         this.content = this._getStateFromHash()
         updateCb(this.content)
         this._onHashChange = function() {
-            if(DEBUG) console.log("DASH: [STATE] Hash changed",this._id,this._content)
-            this.content = this._getStateFromHash()
-            if(updateCb) updateCb(this.content)
+            if(this.content != this._getStateFromHash() ) {
+                this.content = this._getStateFromHash()
+                if(DEBUG) console.log("DASH: [STATE] Hash changed",this._id,this._content)
+                if(updateCb) updateCb(this.content)
+            }
         }.bind(this)
         window.addEventListener('hashchange', this._onHashChange, true);
     }
@@ -32,33 +34,43 @@ class ComponentStatePersistence {
     _getStateFromHash() {
         const hashParts = window.location.hash.split("/")
         const [name, ...rest] = hashParts[2].split(":")
-        let statesInHash = rest.length > 1 ? JSON.parse(decodeURIComponent(rest.join(":"))) : {}
-        return statesInHash[this._id]
+        try {
+            let statesInHash = rest.length > 1 ? JSON.parse(decodeURIComponent(rest.join(":"))) : {}
+            return statesInHash[this._id]
+        } catch (e) {
+            if(DEBUG) console.error("DASH: [STATE] invalid parse of hash:", decodeURIComponent(rest.join(":")))
+            return undefined
+        }
     }
 
     _setStateInHash() {
         const hashParts = window.location.hash.split("/")
         const [name, ...rest] = hashParts[2].split(":")
-        let statesInHash = rest.length > 1 ? JSON.parse(decodeURIComponent(rest.join(":"))) : {}
+        try {
+            let statesInHash = rest.length > 1 ? JSON.parse(decodeURIComponent(rest.join(":"))) : {}
 
-        // Update if content changed and we are still in a dash custom-UI
-        if(JSON.stringify(statesInHash[this._id]) !== JSON.stringify(this._content) && hashParts[3] && hashParts[3].startsWith("dash") ) {
-            // Update id property or remove it, if content is empty
-            if( this._content ) {
-                statesInHash[this._id] = this._content
-            } else {
-                delete statesInHash[this._id]
+            // Update if content changed and we are still in a dash custom-UI
+            if(JSON.stringify(statesInHash[this._id]) !== JSON.stringify(this._content) && hashParts[3] && hashParts[3].startsWith("dash") ) {
+                // Update id property or remove it, if content is empty
+                if( this._content ) {
+                    statesInHash[this._id] = this._content
+                } else {
+                    delete statesInHash[this._id]
+                }
+        
+                hashParts[2] = Object.keys(statesInHash).length > 0 ? `${name}:${JSON.stringify(statesInHash)}` : name
+        
+                const newDestination = hashParts.join("/")
+                if(history.pushState) {
+                    history.pushState(null, null, newDestination);
+                }
+                else {
+                    location.hash = newDestination;
+                }
             }
-    
-            hashParts[2] = Object.keys(statesInHash).length > 0 ? `${name}:${JSON.stringify(statesInHash)}` : name
-    
-            const newDestination = hashParts.join("/")
-            if(history.pushState) {
-                history.pushState(null, null, newDestination);
-            }
-            else {
-                location.hash = newDestination;
-            }
+        }
+        catch (e) {
+            if(DEBUG) console.error("DASH: [STATE] invalid parse of hash:", decodeURIComponent(rest.join(":")))
         }
     }
 }
