9a61abc477ab36e72c201e013487e0d6e1af3588
Miguel Mesquita <miguel.a.mesquita@gmail.com>
2023-06-23 01:47:19 +0100

    fix: corrects remaining bug on load/unload of dash

--- customize.js
+++ customize.js
@@ -1,3 +1,3 @@
 module.exports = {
-  version: "6.1.1",
+  version: "6.1.2",
 };
--- recordm/customUI/dash/src/main.js
+++ recordm/customUI/dash/src/main.js
@@ -3,10 +3,21 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-console.debug("DASHMAIN: Creating vue for the dashboard");
-let vueApp = new Vue({
-    render: function(h) { return h(App); },
-}).$mount("#app");
+debugger
+if(typeof(window.dashActive) == 'undefined') {
+    window.dashActive = false
+}
+
+let vueApp
+if (!window.dashActive) {
+    console.debug("DASHMAIN: Creating vue for the dashboard");
+    vueApp = new Vue({
+        render: function(h) { return h(App); },
+    }).$mount("#app");
+    window.dashActive = true
+} else {
+    console.debug("DASHMAIN: Not creating vue for the dashboard (will be done by the handler)"); 
+}
 
 function getDashName() {
     const dashParts = window.location.hash.split("/")
@@ -14,21 +25,27 @@ function getDashName() {
 }
 
 const initialDashName = getDashName()
-let dashActive = true
+
 
 function onHashChange() {
     const currentDashName = getDashName()
-    if (currentDashName !== initialDashName && dashActive ) {
+    if (currentDashName !== initialDashName && window.dashActive ) {
         console.debug("DASHMAIN: Leaving the initialDashName=",initialDashName," for currentDashName=",currentDashName,". Destroying the dashboard");
         vueApp.$destroy();
-        dashActive = false
-    } 
-    if (currentDashName === initialDashName && !dashActive ) {
-        dashActive = true
-        console.debug("DASHMAIN: Re-Creating vue for the dashboard");
-        vueApp = new Vue({
-            render: function(h) { return h(App); },
-        }).$mount("#app");        
+        window.dashActive = false
+    } else if (currentDashName === initialDashName && !window.dashActive) {
+        if(document.getElementById("app")) {
+            console.debug("DASHMAIN: Re-Creating vue for the dashboard");
+            vueApp = new Vue({
+                render: function(h) { return h(App); },
+            }).$mount("#app"); 
+
+        } else {
+            console.debug("DASHMAIN: #app not ready. Wait a little ");
+            var div = document.createElement("div");
+            document.getElementById("dash").appendChild(div);                   
+            setTimeout( onHashChange, 100 )
+        }
     }
 }
 
