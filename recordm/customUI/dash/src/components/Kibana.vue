b00aa8be1e07a80919594fa5a105e20ff31bb04a
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2024-03-20 15:42:23 +0000

    Fix kibana output filter when user selects this week or this day
    The way the query was built it wouldn't return any values because all
    records would be excluded when lower than date. if both start date and
    end date are the same then it must lower then and equals to  ...

--- recordm/customUI/dash/src/components/Kibana.vue
+++ recordm/customUI/dash/src/components/Kibana.vue
@@ -92,8 +92,16 @@
                   let maxDate = Date.parse(event.data.time.max)
                   if (isNaN(maxDate)) maxDate = event.data.time.max.replace('/', '\\/')
 
-                  if (event.data.time.min) timeQuery += `${this.kibanaTimeField}.date:>=${minDate} `;
-                  if (event.data.time.max) timeQuery += `${this.kibanaTimeField}.date:<${maxDate} `;
+                  // Aparently when filtering results like 'today, this week' what kibana is doing is "<="
+                  const includeMaxDateResults = minDate === maxDate
+
+                  // Keep it retro compatible
+                  const timeField = this.kibanaTimeField.trim().endsWith(".date")
+                      ? this.kibanaTimeField.trim()
+                      : this.kibanaTimeField.trim() + ".date"
+
+                  if (event.data.time.min) timeQuery += `${timeField}:>=${minDate} `;
+                  if (event.data.time.max) timeQuery += `${timeField}:${includeMaxDateResults ? '<=' : '<'}${maxDate} `;
                 }
 
                 // Só vale a pena reagir aos eventos de mudança de filtro no Kibana
