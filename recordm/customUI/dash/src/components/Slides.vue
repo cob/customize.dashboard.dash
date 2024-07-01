ab7c65a238c7631bd36c7cac2ffbf4ee75cd4a89
Ricardo Valverde <ricardovalverde2000@gmail.com>
2024-07-01 17:36:56 +0100

    fix:slides reveal init state robustness fix

--- recordm/customUI/dash/src/components/Slides.vue
+++ recordm/customUI/dash/src/components/Slides.vue
@@ -35,7 +35,7 @@ export default {
     },
     data: () => ({
         reachedEnd: false,
-        myDeck: null,
+        myDeck: undefined,
         status: "loading_component",
     }),
     computed: {
