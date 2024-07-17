1df96b1777c02fd0199a4885f8476cd30346b740
Daniel João <daniel.joao@cultofbits.com>
2024-07-17 10:07:24 +0100

    Adds 3 helper functions, greaterThan, lesserThan, and objectFieldValue

--- recordm/customUI/dash/src/App.vue
+++ recordm/customUI/dash/src/App.vue
@@ -132,6 +132,15 @@
     } else {
       return options.inverse(this);
     }});
+  Handlebars.registerHelper('greaterThan', function(arg1,arg2) {
+    return ((arg1?arg1*1:0) > arg2*1); 
+  });
+  Handlebars.registerHelper('lesserThan', function(arg1,arg2) {
+    return ((arg1?arg1*1:0) < arg2*1); 
+  });
+  Handlebars.registerHelper('objectFieldValue', function(vars,key,value) {
+    return vars[key] ? vars[key] : value
+  })
   Handlebars.registerHelper('dateInfoTimestamp', function(timestamp, keyword) {
     if(!timestamp)
       return "No date."
