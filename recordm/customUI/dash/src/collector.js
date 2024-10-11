bfa6b3cd731a1d8bafedeabb2d68ba5a61f0009c
Ricardo Valverde <ricardovalverde2000@gmail.com>
2024-10-11 16:28:00 +0100

    added new output var option to calendar that stores the start and end date of the calendar in milliseconds for complex queries

--- recordm/customUI/dash/src/collector.js
+++ recordm/customUI/dash/src/collector.js
@@ -179,6 +179,7 @@ function parseDashboard(raw_dashboard) {
                 "CalendarClasses": "",
                 "InputVarCalendar": [{}],
                 "OutputVarCalendar": "",
+                "OutputVarInterval": "",
                 "MaxVisibleDayEvents": "",
                 "AllowCreateInstances":"",
                 "CreateDefinition":"",
