55cbc85c53064f689c48b6b58d6323bc4b5bb0d1
Ricardo Valverde <ricardovalverde2000@gmail.com>
2025-01-23 15:58:23 +0000

    removed duplicate import in customizations2

--- recordm/customUI/js/customizations2.__MERGE__.js
+++ recordm/customUI/js/customizations2.__MERGE__.js
@@ -3,4 +3,3 @@ import "./lib/tailwind.3.3.2.js";
 import "./cob/_dash.js";
 import "./cob/_dashboards_save_and_edit.js";
 import "./cob/_dash_customizations.js"
-import "./cob/_dash_customizations.js";
